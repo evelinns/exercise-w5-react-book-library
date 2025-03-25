7886a01bad9f5db3c416e2c38ad94422c3d20238
evelinns <evelinn.idenfors@gmail.com>
2025-03-25 18:06:04 +0000

    added header and base styling of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Header } from "./components/Header";
 import data from "./data.json";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <div>Book library goes here</div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return <div className="header">
+    <h1>Library</h1>
+  </div>
+}
